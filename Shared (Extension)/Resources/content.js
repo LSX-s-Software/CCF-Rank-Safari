c2bc5d67972cb55c8a330de9f239c86d033fd711
LSX-s-Software <2682191498@qq.com>
2022-04-22 23:10:36 +0800

    chore: Initial Commit

--- Shared (Extension)/Resources/content.js
+++ Shared (Extension)/Resources/content.js
@@ -1,7 +1,21 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
-
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+if (location.href.startsWith("https://ieeexplore.ieee.org/search/searchresult.jsp?")) {
+  ieee.rankingSpanProvider.push(ccf.getRankingSpan);
+  ieee.start();
+} else if (location.href.startsWith("https://dl.acm.org/action/doSearch?")) {
+  acm.rankingSpanProvider.push(ccf.getRankingSpan);
+  acm.start();
+} else if (location.hostname.startsWith("dblp")) {
+  ccf.custom2rank = dblp.uri2rank;
+  dblp.rankingSpanProvider.push(ccf.getRankingSpan);
+  dblp.start();
+} else if (location.href.startsWith("https://link.springer.com/search")) {
+  springer.rankingSpanProvider.push(ccf.getRankingSpan);
+  springer.start();
+} else if (location.href.startsWith("https://www.engineeringvillage.com/search")) {
+  ei.rankingSpanProvider.push(ccf.getRankingSpan);
+  ei.start();
+} else if (location.hostname.startsWith("scholar.google")) {
+  ccf.custom2rank = dblp.uri2rank;
+  scholar.rankSpanList.push(ccf.getRankingSpan);
+  scholar.run();
+}
