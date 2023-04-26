f9e44b40793998bae3a2c639ef7b5dab128d258c
LSX-s-Software <2682191498@qq.com>
2023-04-26 20:18:18 +0800

    feat: popup支持通过手动输入来查询等级

--- Shared (Extension)/Resources/popup.js
+++ Shared (Extension)/Resources/popup.js
@@ -1,1 +1,13 @@
+function displayResult(result) {
+  const resultDiv = document.querySelector("#result");
+  resultDiv.innerHTML = `<span>查询结果：</span>`;
+  resultDiv.appendChild(result);
+}
 
+document.querySelector("#search").addEventListener("click", (e) => {
+  e.preventDefault();
+  const query = document.querySelector("#input").value;
+  if (query) {
+    displayResult(ccf.getRankingSpan([{full: query, abbr: query}]));
+  }
+});
